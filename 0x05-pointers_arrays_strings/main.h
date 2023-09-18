bff69518fbbf8d1f0cf9ebbd7d6219485fba7ef8
hamzaoutayb123 <bnjmbarka1953@gmail.com>
2023-09-18 15:30:48 -0700

    changes

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,17 +1,19 @@
-#ifndef "main.h"
-#define "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
 void reset_to_98(int *n);
-int _atoi(char *s);
-char *_strcpy(char *dest, char *src);
-void print_array(int *a, int n);
-void puts_half(char *str);
-void puts2(char *str);
-void rev_string(char *s);
-void print_rev(char *s);
-void _puts(char *str);
-int _strlen(char *s);
 void swap_int(int *a, int *b);
-void reset_to_98(int *n);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 
 #endif
