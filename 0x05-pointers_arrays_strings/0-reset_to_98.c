416942fd144a7e0f64b4d9708ba722d9ba557cac
hamzaoutayb123 <bnjmbarka1953@gmail.com>
2023-09-18 02:45:51 -0700

    first commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 -  a function that takes a pointer to an int parameter
+ *
+ * @n: pointer type int
+*/
+
+void reset_to_98(int *n)
+{
+	int *n = 98;
+
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef "main.h"
+#define "main.h"
+
+void reset_to_98(int *n);
+int _atoi(char *s);
+char *_strcpy(char *dest, char *src);
+void print_array(int *a, int n);
+void puts_half(char *str);
+void puts2(char *str);
+void rev_string(char *s);
+void print_rev(char *s);
+void _puts(char *str);
+int _strlen(char *s);
+void swap_int(int *a, int *b);
+void reset_to_98(int *n);
+
+#endif
