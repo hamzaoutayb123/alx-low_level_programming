166872bd5dbbbbf3dbe5e72f0a081287a528475e
hamzaoutayb123 <bnjmbarka1953@gmail.com>
2023-10-02 08:38:31 -0700

    qwerty

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that multiplies two numbers.
+ *
+ * @argc: int
+ * @argv: pointer to an array of command line arguments.
+ *
+ * Return: Return (0) success , if it didnt it fails.
+*/
+
+int main(int argc, char *argv[])
+{
+	int darb;
+
+	if (argc == 3)
+	{
+		darb = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", darb);
+	}
+	else
+	{
+		printf("you have problem in input\n");
+		return (1);
+	}
+	return (0);
+}
+
