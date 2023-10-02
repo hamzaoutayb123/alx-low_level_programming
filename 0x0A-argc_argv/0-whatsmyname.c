bb8493c493a864327e5353636c9be7b2cad1e154
hamzaoutayb123 <bnjmbarka1953@gmail.com>
2023-10-02 04:12:33 -0700

    qwerty

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints its name.
+ *
+ * @argc: int
+ * @argv: list
+ *
+ * Return: Always return (0)
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s \n", argv[0]);
+	return (0);
+}
